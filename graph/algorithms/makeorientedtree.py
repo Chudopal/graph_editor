c1ee5c0f967ae8fa566d186b396bb927716f97e7
chudopal <chudopal.00@mail.ru>
2020-04-28 19:18:46 +0300

    it works only for delete in oritnted graphs

--- graph/algorithms/makeorientedtree.py
+++ graph/algorithms/makeorientedtree.py
@@ -20,42 +20,27 @@ def make_tree(incedent_matrix, current_vertex):
     if not current_vertex:
         USED = [x*0 for x in range(len(incedent_matrix))]
     USED[current_vertex] = 1
+    print(incedent_matrix[current_vertex])
     for edge_by in range(len(incedent_matrix[current_vertex])):
         if incedent_matrix[current_vertex][edge_by]:
             numb_of_edges += 1
-        if edge_by > current_vertex:
-            if incedent_matrix[current_vertex][edge_by]:
-                if USED[edge_by]:
-                    incedent_matrix[current_vertex][edge_by] = 0;
-                    incedent_matrix[edge_by][current_vertex] = 0;
-                elif not USED[edge_by]:
-                    make_tree(incedent_matrix, edge_by)
-    if numb_of_edges == 1:
-        LEAVES.append(current_vertex)
-    if numb_of_edges == 0:
-        LONELY_VERTEXES.append(current_vertex)
-    if not LEAVES:
-        for lonely_vertex in LONELY_VERTEXES:
-            if lonely_vertex != len(incedent_matrix) -1:
-                incedent_matrix[lonely_vertex][lonely_vertex + 1] = 1
-            if lonely_vertex != 0:
-                incedent_matrix[lonely_vertex][lonely_vertex - 1] = 1
-    else:
-        for lonely_vertex in LONELY_VERTEXES:
-            incedent_matrix[LEAVES[0]][lonely_vertex] = 1
-            incedent_matrix[lonely_vertex][LEAVES[0]] = 1
+            if USED[edge_by]:
+                print("hello world")
+                incedent_matrix[current_vertex][edge_by] = 0;
+            else:
+                make_tree(incedent_matrix, edge_by)
     return incedent_matrix
 
 def tree(incedent_matrix):
     new_matrix()
     incedent_matrix = make_tree(incedent_matrix, 0)
-    for vertex in range(len(USED)):
-        if not USED[vertex]:
-            incedent_matrix = make_tree(incedent_matrix, vertex)
+    print(USED)
     return incedent_matrix
 
-if __name__ == "__main__":
 
+
+
+if __name__ == "__main__":
     incedent_matrix = [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
@@ -74,7 +59,7 @@ if __name__ == "__main__":
         print("ok")
     else:
         print("no")
-        print(tree(incedent_matrix))
+    #    print(tree(incedent_matrix))
     new_matrix()
     incedent_matrix = [
         [0, 0, 0],
@@ -87,9 +72,9 @@ if __name__ == "__main__":
         [0, 0, 0],
         [0, 0, 0],
     ]
+    #print("HERE",tree(incedent_matrix))
     if right_matrix == tree(incedent_matrix):
         print("ok")
-        print(tree(incedent_matrix))
     else:
         print("no")
     new_matrix()
@@ -110,7 +95,7 @@ if __name__ == "__main__":
         print("ok")
     else:
         print("no")
-        print(tree(incedent_matrix))
+    #    print(tree(incedent_matrix))
     new_matrix()
     incedent_matrix = [
         [0, 1, 1, 1, 0],
@@ -132,7 +117,7 @@ if __name__ == "__main__":
         print("ok")
     else:
         print("no")
-        print(tree(incedent_matrix))
+        #print(tree(incedent_matrix))
     incedent_matrix = [
         [0, 1, 1],
         [0, 0, 1],
@@ -146,4 +131,6 @@ if __name__ == "__main__":
     ]
     if right_matrix == tree(incedent_matrix):
         print("ok")
-        print(tree(incedent_matrix))
+    print("//////////////////////////")
+    print(tree(incedent_matrix))
+    #    print(tree(incedent_matrix))
