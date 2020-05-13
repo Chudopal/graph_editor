77a71e7eb16e2672aa03aba7425fa7472df02696
chudopal <chudopal.00@mail.ru>
2020-05-13 23:20:29 +0300

    begined work with reqests

--- graph/static/graph/js/draw.js
+++ graph/static/graph/js/draw.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+$(document).ready(function(){
   ns = 'http://www.w3.org/2000/svg';
   var draw = document.getElementById("draw");
   var names = document.getElementById("names");
@@ -13,6 +13,54 @@ window.onload = function(){
   var choseCircle = new ChoseCircle();
   var graphsInformation = document.getElementById("layer");
   var graph = new Graph();
+
+  $("#here").on("click", function(){
+    makeRequest("localhost:8989/edit-graph/graph")
+  })
+
+
+  function makeRequest(url) {
+    var httpRequest = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+            // Читайте ниже об этой строке
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Не вышло :( Невозможно создать экземпляр класса XMLHTTP ');
+        return false;
+    }
+    httpRequest.onreadystatechange = function() { alertContents(httpRequest); };
+    httpRequest.open('GET', url, true);
+    httpRequest.send(null);
+  }
+
+
+
+  function alertContents(httpRequest) {
+
+     if (httpRequest.readyState == 4) {
+         if (httpRequest.status == 200) {
+             alert(httpRequest.responseText);
+         } else {
+             alert('С запросом возникла проблема.');
+         }
+      }
+
+  }
+
 
 
   function Graph(){
@@ -1019,6 +1067,7 @@ window.onload = function(){
     }
   }
 
+  
   function animateColorPanel(beginPosition, finalPosition, isEdge, xPosition, yPosition, beginRadius, endRadius){
     var start = Date.now();
     var move = finalPosition - beginPosition;
@@ -1098,6 +1147,7 @@ window.onload = function(){
   }
 
 
+
   unorientedButton.addEventListener("click", clear);
   unorientedButton.addEventListener("click", (e)=>{
 
@@ -1121,4 +1171,4 @@ window.onload = function(){
     }
     isCreating = true;
   });
-}
+})
--- graph/views.py
+++ graph/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from graph.models import OrientedGraph
 # Create your views here.
 
 def canvas_view(request):
     return render(request, "graph/background.xhtml")
+
+
+def current_graph(request):
+    return HttpResponse("hello")
