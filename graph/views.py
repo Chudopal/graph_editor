0ab2032cd5da9bda89244a8fd1b5b0ccaf16ca22
chudopal <chudopal.00@mail.ru>
2020-05-17 01:24:06 +0300

    creatin prototype of each view

--- graph/views.py
+++ graph/views.py
@@ -6,20 +6,37 @@ import json
 # Create your views here.
 
 def canvas_view(request):
+    """Function for main view
+
+    This function is building the canvas and
+    all, what the user can see.
+    """
     return render(request, "graph/background.xhtml")
 
 
 def save_graph(request):
+    """Function for saving graph
+
+    This function allows to save 
+    graph into server.
+    """
     structure = request.GET.dict()
     print(structure["id"])
     graph = Graph.objects.get(id=int(structure["id"]))
+    graph.name = structure["name"]
     with open(graph.path_to_graph, 'w') as f:
         json.dump(structure, f)
         f.close() 
+    graph.save()
     return HttpResponse()
 
 
 def new_graph(request):
+    """Function for creating new graph
+
+    This function allows to ceating a new record
+    in the database.
+    """
     graph = Graph()
     graph.save()
     path_to_graph = "graph/graphs/graph" + str(graph.id) + ".json" 
@@ -30,3 +47,102 @@ def new_graph(request):
     graph.path_to_graph = path_to_graph
     graph.save()
     return HttpResponse(graph.id)
+
+
+def get_graph(request):
+    """Function for getting graph
+
+    This function allows to get the 
+    current graph from server.
+    """
+    pass
+
+
+def get_list_of_graphs(request):
+    """Function for getting a list of all graphs
+
+    This function return list of graphs, which is in server.
+    """
+    pass
+
+
+def is_tree(request):
+    """Function for determination is graph tree or no
+
+    This function is getting a graph and response:
+    "1" -- it is a tree,
+    "0" -- it is not a tree.
+    """
+    pass
+
+
+def make_tree(request):
+    """Function for making a tree
+
+    This function is getting a graph,
+    and returns a tree or "0".
+    """
+    pass
+
+
+def make_binary_tree(request):
+    """Function for making a binary tree
+
+    This function is getting a graph,
+    and returns a binary tree or "0".
+    """
+    pass
+
+
+def find_hamilton_cycle(request):
+    """Function for finding a Hamilton cycle
+
+    This function is getting a graph and returns 
+    his Hamelton cycle or "0".
+    """
+    pass
+
+
+def find_diameter(request):
+    """Function for find a diameter of a graph
+
+    This function is getting a graph and returns 
+    his diameter or "0".
+    """
+    pass
+
+
+def find_radius(request):
+    """Function for find a radius of a graph
+
+    This function is getting a graph and returns 
+    his radius or "0".
+    """
+    pass
+
+
+def find_centre():
+    """Function for find a centre of a graph
+
+    This function is getting a graph and returns 
+    his centre or "0".
+    """
+    pass
+
+
+def vector_product():
+    """Funstion for calculating a vector product of 2 graphs
+
+    This function is getting 2 graphs and return their vector
+    product
+    """
+    pass
+
+
+def cartesian_product():
+    """Funstion for calculating a cartesian product of 2 graphs
+
+    This function is getting 2 graphs and return their cartesian
+    product
+    """
+    pass
