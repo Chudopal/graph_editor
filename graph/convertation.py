0340fb23c32393bc8873a4d098ada613fb00940e
chudopal <chudopal.00@mail.ru>
2020-05-20 19:26:47 +0300

    make convertation to matrix

--- graph/convertation.py
+++ graph/convertation.py
@@ -9,17 +9,14 @@ def to_matrix(graph):
         [0, 1, 0],
     ].
     """
-    graph_view = {
-        "graph_json": graph,
-        "number_of_noodes":[
-
-        ],
-        "matrix":[
-
-        ]
-    }
-    print("HERE IS NAME OF VERTEX", len(graph["nodes"]))
-    #return graph_view
+    graph_names = [node["name"] for node in graph["nodes"]]
+    numb_of_nodes = len(graph_names)
+    matrix = [[0] * numb_of_nodes for i in range(0, numb_of_nodes)]
+    for edge in graph["edges"]:
+        first_node = graph_names.index(edge["firstNode"])
+        second_node = graph_names.index(edge["secondNode"])
+        matrix[first_node][second_node] = 1
+    return matrix
 
 
 def to_json(matrix):
